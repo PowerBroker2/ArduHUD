e3fab37be472d0dc270681c50563be2132968739
PB2 <gitstuff2@gmail.com>
2020-03-17 14:37:04 -0400

    Add wifi AP webserver functionality

--- src/ESP32_HUD/ESP32_HUD.ino
+++ src/ESP32_HUD/ESP32_HUD.ino
@@ -1,3 +1,6 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
 #include "BluetoothSerial.h"
 #include "SerialTransfer.h"
 #include "ELMduino.h"
@@ -9,6 +12,11 @@ BluetoothSerial SerialBT;
 #define ELM_PORT        SerialBT
 
 
+const char *ssid = "HUDuino";
+const char MAIN_page[] PROGMEM = "<!DOCTYPE html>\n<html>\n<style>\nbody, html {\n  height: 100%;\n  margin: 0;\n  font-family: Arial;\n}\n\n/* Style tab links */\n.tablink {\n  background-color: #555;\n  color: white;\n  float: left;\n  border: none;\n  outline: none;\n  cursor: pointer;\n  padding: 14px 16px;\n  font-size: 17px;\n  width: 33.33%;\n}\n\n.tablink:hover {\n  background-color: #777;\n}\n\n.tabcontent {\n  color: white;\n  display: none;\n  padding: 100px 20px;\n  height: 100%;\n}\n\n#Home {background-color: white;}\n#PIDs {background-color: white;}\n#Console {background-color: white;}\n#About {background-color: white;}\n\n.card\n{\n    max-width: 400px;\n     min-height: 250px;\n     background: #02b875;\n     padding: 30px;\n     box-sizing: border-box;\n     color: #FFF;\n     margin:20px;\n     box-shadow: 0px 2px 18px -4px rgba(0,0,0,0.75);\n}\n\n#myProgress\n{\n  width: 100%;\n  background-color: #ddd;\n}\n\n#myBar\n{\n  width: 1%;\n  height: 30px;\n  background-color: #4CAF50;\n}\n\n/* Style the form - display items horizontally */\n.form-inline {\n  display: flex;\n  flex-flow: row wrap;\n  align-items: center;\n}\n\n/* Add some margins for each label */\n.form-inline label {\n  margin: 5px 10px 5px 0;\n}\n\n/* Style the input fields */\n.form-inline input {\n  vertical-align: middle;\n  margin: 5px 10px 5px 0;\n  padding: 10px;\n  background-color: #fff;\n  border: 1px solid #ddd;\n}\n\n/* Style the submit button */\n.form-inline button {\n  padding: 10px 20px;\n  background-color: dodgerblue;\n  border: 1px solid #ddd;\n  color: white;\n}\n\n.form-inline button:hover {\n  background-color: royalblue;\n}\n\n/* Add responsiveness - display the form controls vertically instead of horizontally on screens that are less than 800px wide */\n@media (max-width: 800px) {\n  .form-inline input {\n    margin: 10px 0;\n  }\n\n  .form-inline {\n    flex-direction: column;\n    align-items: stretch;\n  }\n}\n</style>\n\n<body>\n<button class=\"tablink\" onclick=\"openPage('Home', this, 'red')\" id=\"defaultOpen\">Home</button>\n<button class=\"tablink\" onclick=\"openPage('PIDs', this, 'green')\">PIDs</button>\n<button class=\"tablink\" onclick=\"openPage('Console', this, 'blue')\">Console</button>\n\n<div id=\"Home\" class=\"tabcontent\">\n  <h3>Home</h3>\n  <div class=\"card\">\n    <h4>ArduHUD</h4><br>\n    <h1>Speed: <span id=\"speed\">0</span></h1>\n    <div id=\"myProgress\">\n      <div id=\"myBar\"></div>\n    </div><br>\n  </div>\n</div>\n\n<div id=\"PIDs\" class=\"tabcontent\">\n  <h3>PIDs</h3>\n  <p>Some PIDs this fine day!</p>\n</div>\n\n<div id=\"Console\" class=\"tabcontent\">\n  <h3>Console</h3>\n  <form class=\"form-inline\" action=\"/action_page.php\">\n    <label for=\"email\">Email:</label>\n    <input type=\"email\" id=\"email\" placeholder=\"Enter email\" name=\"email\">\n    <button type=\"submit\">Submit</button>\n</form>\n</div>\n</body>\n\n<script>\nfunction openPage(pageName, elmnt, color)\n{\n  // Hide all elements with class=\"tabcontent\" by default */\n  var i, tabcontent, tablinks;\n  tabcontent = document.getElementsByClassName(\"tabcontent\");\n  for (i = 0; i < tabcontent.length; i++)\n  {\n    tabcontent[i].style.display = \"none\";\n  }\n\n  // Remove the background color of all tablinks/buttons\n  tablinks = document.getElementsByClassName(\"tablink\");\n  for (i = 0; i < tablinks.length; i++)\n  {\n    tablinks[i].style.backgroundColor = \"\";\n  }\n\n  // Show the specific tab content\n  document.getElementById(pageName).style.display = \"block\";\n\n  // Add the specific color to the button used to open the tab content\n  elmnt.style.backgroundColor = color;\n}\n\ndocument.getElementById(\"defaultOpen\").click();\n\nsetInterval(function()\n{\n  getSpeedData();\n  getRpmData();\n}, 100);\n\n\nfunction getSpeedData()\n{\n  var xhttp = new XMLHttpRequest();\n  \n  xhttp.onreadystatechange = function()\n  {\n    if (this.readyState == 4 && this.status == 200)\n      {\n      document.getElementById(\"speed\").innerHTML = this.responseText;\n    }\n  };\n  \n  xhttp.open(\"GET\", \"speed\", true);\n  xhttp.send();\n}\n\n\nfunction move(width)\n{\n  var elem = document.getElementById(\"myBar\");\n  var id = setInterval(frame, 10);\n  \n  function frame()\n  {\n    if (width > 100)\n    {\n      width = 100;\n    }\n    else if (width < 0)\n    {\n      width = 0;\n    }\n    \n    console.log(width);\n    elem.style.width = width + \"%\";\n  }\n}\n\nfunction getRpmData()\n{\n  var xhttp = new XMLHttpRequest();\n  \n  xhttp.onreadystatechange = function()\n  {\n    if (this.readyState == 4 && this.status == 200)\n       {\n       move(parseInt(this.responseText), 10);\n    }\n  };\n  \n  xhttp.open(\"GET\", \"rpm\", true);\n  xhttp.send();\n}\n</script>\n</html>";
+
+
+WiFiServer server(80);
 SerialTransfer myTransfer;
 ELM327 myELM327;
 
@@ -88,6 +96,98 @@ void loop()
 
   myTransfer.txObj(carTelem, sizeof(carTelem));
   myTransfer.sendData(sizeof(carTelem));
+
+  serverProcessing();
+}
+
+
+void serverProcessing()
+{
+  bool mainPage = false;
+  bool speedCall = false;
+  bool rpmCall = false;
+  
+  WiFiClient client = server.available();
+
+  if (client)
+  {
+    String currentLine = "";
+    
+    while (client.connected())
+    {
+      if (client.available())
+      {
+        char c = client.read();
+        //Serial.write(c);
+
+        if (currentLine.endsWith("GET / HTTP"))
+          mainPage = true;
+        else if (currentLine.endsWith("GET /speed HTTP"))
+          speedCall = true;
+        else if (currentLine.endsWith("GET /rpm HTTP"))
+          rpmCall = true;
+        
+        if (c == '\n')
+        {
+          if (!currentLine.length())
+          {
+            if (mainPage)
+            {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println();
+        
+              client.print(MAIN_page);
+              client.println();
+      
+              mainPage = false;
+              break;
+            }
+            else if (speedCall)
+            {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println();
+        
+              client.println(int(carTelem.mph + 0.5));
+              client.println();
+      
+              speedCall = false;
+              break;
+            }
+            else if (rpmCall)
+            {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println();
+        
+              client.println(carTelem.rpm);
+              client.println();
+      
+              rpmCall = false;
+              break;
+            }
+            else
+            {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-type:text/html");
+              client.println();
+        
+              client.print(MAIN_page);
+              client.println();
+              break;
+            }
+          }
+          else
+            currentLine = "";
+        }
+        else if (c != '\r')
+          currentLine += c;
+      }
+    }
+
+    client.stop();
+  }
 }
 
 
@@ -117,4 +217,3 @@ void printError()
 
   delay(100);
 }
-
